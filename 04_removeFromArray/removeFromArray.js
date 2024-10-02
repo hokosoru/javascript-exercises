558ff9b6b5b78176c0fcadd4c493b14a7c86a839
Añero <132313415+hokosoru@users.noreply.github.com>
2024-10-02 21:29:25 +0800

    Done Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr1, num) {
+    const array = arr1;
+    const index = array.indexOf(num);
+    for(let i = 0; i < array.length; i++){
+        if(i == num){
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
