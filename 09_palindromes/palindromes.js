bbca45e064c63a3e61dcf73eab35c9e7c6b77617
Añero <132313415+hokosoru@users.noreply.github.com>
2024-10-03 22:54:24 +0800

    Done Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    let lowerCase = palindrome.toLowerCase().replace(/[^a-z0-9]/g, "");
+    
+    let reverse = lowerCase
+    .split("")
+    .reverse()
+    .join("");
 
+    if(reverse == lowerCase){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
