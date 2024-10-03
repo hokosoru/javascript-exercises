ca768ce5c156ea06834bc7db16a7340faf8080c7
Añero <132313415+hokosoru@users.noreply.github.com>
2024-10-03 18:58:46 +0800

    Done exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    const century = year % 100 == 0;
+    const yearDivideby400 = year % 400 == 0;
+    if(year % 4 == 0 && ( !century || yearDivideby400 )){
+        return true;
+    }else{
+        return false;
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
 };
 
 // Do not edit below this line
