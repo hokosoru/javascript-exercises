0b24c464872e3970ba00733ed4d8ffc45483c1e2
Añero <132313415+hokosoru@users.noreply.github.com>
2024-10-03 23:11:11 +0800

    Done Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    let num = parseInt(fib);
+    let n1 = 0
+        ,n2 = 1;
+    let result;
 
-};
+    if(num === 0) return 0;
+    if(num === 1) return 1;
+    if(num < 0) return "OOPS";
+
+    for(let i = 2; i <= num; i++){
+        result = n1 + n2;
+        n1 = n2;
+        n2 = result;
+    }
+    
+    return result;
+};  
 
 // Do not edit below this line
 module.exports = fibonacci;
